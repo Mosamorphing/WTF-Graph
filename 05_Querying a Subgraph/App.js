2672983b2269a39995426698ed99e66e791cb0d8
Mosamorphing <121625762+Mosamorphing@users.noreply.github.com>
2024-07-08 09:07:38 -0700

    Update App.js

--- 05_Querying a Subgraph/App.js
+++ 05_Querying a Subgraph/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import { createClient, cacheExchange, fetchExchange, gql } from '@urql/core';
 
 const client = createClient({
-  url: 'https://gateway-arbitrum.network.thegraph.com/api/7a9b57751c720ac54c24e20aaba21216/subgraphs/id/2hTKKMwLsdfJm9N7gUeajkgg8sdJwky56Zpkvg8ZcyP8',
+  url: 'https://gateway-arbitrum.network.thegraph.com/api/[api-key]/subgraphs/id/2hTKKMwLsdfJm9N7gUeajkgg8sdJwky56Zpkvg8ZcyP8',
   exchanges: [cacheExchange, fetchExchange],
 });
 
